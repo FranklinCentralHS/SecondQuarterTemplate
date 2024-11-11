75d7b1741670d9e9c21be6431759923d5f02b018
owilling <owilling@ftstudent.org>
2024-11-11 13:52:40 -0500

    finnished slime enemy
    **speeds still may be need to be tweaked

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,12 +1,22 @@
 extends Node2D
 
-var speed = 25
+#higher the speed var slower the enemy
+@export var speed = 100
 var player_chase = false
 var player = null
+@onready var sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float):
 	if player_chase == true:
-		position += (player.position - position)
+		position += (player.position - position)/speed
+
+		sprite.play("moving")
+		if(player.position.x - position.x) < 0:
+			sprite.flip_h = true
+		else:
+			sprite.flip_h = false
+	else:
+		sprite.play("idle")
 
 func _on_detection_area_body_entered(body:Node2D) -> void:
 	player = body
