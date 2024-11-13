3add082bd8af16165a6e59b102a3edd9c4055bbb
BryceM7 <brycem417@gmail.com>
2024-11-13 14:04:31 -0500

    Animated heart for health pickup

---
+++ scripts/HealthBar.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+class_name UI
+@onready var rectangle: TextureProgressBar = $health_bar
+@export var health : int
+
+func _on_ready():
+	rectangle.max_value = health
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
