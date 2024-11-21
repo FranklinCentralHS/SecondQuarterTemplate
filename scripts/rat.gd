795ce44535aa79baeee3a77aa715778b581328e1
owilling <owilling@ftstudent.org>
2024-11-21 12:50:14 -0500

    changes

--- scripts/rat.gd
+++ scripts/rat.gd
@@ -4,7 +4,7 @@ func _process(delta: float) -> void:
 	if attacking == false:
 		if player_chase == true:
 			self.position += (player.position - self.position)/speed
-
+			
 			sprite.play("moving")
 			if (player.position.x - self.position.x) > 0:
 				sprite.flip_h = false
@@ -13,16 +13,17 @@ func _process(delta: float) -> void:
 		else:
 			sprite.play("idle")
 
-#changes state of enenmy to chase
+#changes the chase state var
 func _on_detection_area_body_entered(body:Node2D) -> void:
 	self.range_body_entered(body)
 func _on_detection_area_body_exited(body:Node2D) -> void:
 	self.range_body_exited(body)
 
-#checks to see if player is in range of melee attack
+#checks if player is in range of melee attack
 func _on_attack_area_body_entered(body:Node2D) -> void:
 	if(body is Player):
 		self.start_attack_timer()
 func _on_attack_area_body_exited(body:Node2D) -> void:
 	if(body is Player):
 		self.stop_attack_timer()
+		
