5762d83954bb815820f3448223af302cebc1d740
BryceM7 <brycem417@gmail.com>
2024-12-03 13:49:14 -0500

    enemy damges player

---
+++ scripts/enemy.gd
@@ -0,0 +1,53 @@
+extends Node
+class_name enemy
+
+#higher the speed var slower the enemy
+@export var speed = 100
+#higher the speed slower the attack
+@export var attack_speed = 1
+@export var damage = 20
+
+#change of state vars
+var player_chase = false
+var player = null
+var attacking = false
+
+@onready var sprite = $AnimatedSprite2D
+@onready var attackTimer = $attackTimer
+
+
+func _ready() -> void:
+	attackTimer.wait_time = attack_speed
+
+#chases player if in range
+func range_body_entered(body:Node2D):
+	if (body is Player):
+		player = body
+		player_chase = true
+#stops chasing player if out of range
+func range_body_exited(body:Node2D):
+	if(body is Player):
+		player = null
+		player_chase = false
+
+#starts attack cooldown
+func start_attack_timer():
+	attackTimer.start()
+	attacking = true
+
+#stops attack cooldown
+func stop_attack_timer():
+	attackTimer.stop()
+	attacking = false
+
+func attack():
+	sprite.play("attack")
+
+#checks to see if player is in range of melee attack
+func _on_attack_area_body_entered(body:Node2D) -> void:
+	if(body is Player):
+		self.start_attack_timer()
+		body.health -= damage
+func _on_attack_area_body_exited(body:Node2D) -> void:
+	if(body is Player):
+		self.stop_attack_timer()
