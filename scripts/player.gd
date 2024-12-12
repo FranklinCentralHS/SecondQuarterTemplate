0deca0be081262b2f0cccee53b19ce72cc7998a1
owilling <owilling@ftstudent.org>
2024-12-12 12:57:22 -0500

    Merge branch 'main' into energy

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 class_name Player
 
 @onready var healthBar : UI = %HealthBar
+@onready var timer : Timer = $DeathTimer
 
 
 var health : int = 100 :
@@ -10,6 +11,14 @@ var health : int = 100 :
 		health = value
 		if healthBar != null:
 			healthBar.set_health(value)
+		if health <= 0:
+			animations.play("death")
+			is_dying = true
+			if (timer.time_left == 0):
+				timer.start()
+
+
+var is_dying = false
 
 
 enum Directions {UP, DOWN, LEFT, RIGHT}
@@ -24,6 +33,7 @@ enum Directions {UP, DOWN, LEFT, RIGHT}
 var isAttacking = false
 var direction: Vector2 = Vector2.ZERO
 var facing : Directions = Directions.DOWN
+
 
 @onready var animations : AnimatedSprite2D = $AnimatedSprite2D
 
@@ -42,28 +52,28 @@ func _ready() -> void:
 func _process(_delta):
 	direction = Input.get_vector("left", "right", "up", "down")
 	#MOVEMENT
-	if direction.x < 0 && isAttacking == false:
+	if direction.x < 0 && isAttacking == false && not is_dying:
 		animations.play("move_left")
 		facing = Directions.LEFT
-	elif direction.x > 0 && isAttacking == false:
+	elif direction.x > 0 && isAttacking == false && not is_dying:
 		animations.play("move_right")
 		facing = Directions.RIGHT
-	elif direction.y > 0 && isAttacking == false:
+	elif direction.y > 0 && isAttacking == false && not is_dying:
 		animations.play("move_down")
 		facing = Directions.DOWN
-	elif direction.y < 0 && isAttacking == false:
+	elif direction.y < 0 && isAttacking == false && not is_dying:
 		animations.play("move_up")
 		facing = Directions.UP
 	 # Handle idle animations based on direction
-	elif direction.x == 0 && direction.y == 0 && isAttacking == false:
+	elif direction.x == 0 && direction.y == 0 && isAttacking == false && not is_dying:
 		# Player is idle, check facing direction and choose appropriate idle animation
-		if facing == Directions.LEFT && isAttacking == false:
+		if facing == Directions.LEFT && isAttacking == false && not is_dying:
 			animations.play("idle_left")
-		elif facing == Directions.RIGHT && isAttacking == false:
+		elif facing == Directions.RIGHT && isAttacking == false && not is_dying:
 			animations.play("idle_right")
-		elif facing == Directions.DOWN && isAttacking == false:
+		elif facing == Directions.DOWN && isAttacking == false && not is_dying:
 			animations.play("idle_down")
-		elif facing == Directions.UP && isAttacking == false:
+		elif facing == Directions.UP && isAttacking == false && not is_dying:
 			animations.play("idle_up")
 	#Attack Update
 	if Input.is_action_just_pressed("attack"):
@@ -105,6 +115,9 @@ func _on_animated_sprite_2d_animation_finished():
 	if animations.animation == "attack_down" or "attack_up" or "attack_left" or "attack_right":
 		isAttacking = false
 
+
+func _on_timer_timeout() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
 #attacking functoin
 func attack(attackDirection):
 	energy -= 10
